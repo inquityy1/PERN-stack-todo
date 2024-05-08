3c856a77eb286cf4d175c6299d77d8380e3013b8
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-08 16:43:57 +0200

    Edit modal and finishing the entire PERN stack todo list

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
@@ -6,7 +7,7 @@ const ListTodos = () => {
   //delete todo function
   const deleteTodo = async (id) => {
     try {
-      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+      await fetch(`http://localhost:5000/todos/${id}`, {
         method: "DELETE",
       });
 
@@ -51,7 +52,9 @@ const ListTodos = () => {
           {todos.map((todo) => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
               <td>
                 <button
                   className="btn btn-danger"

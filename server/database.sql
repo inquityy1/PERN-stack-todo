a7be0a8c78d24d8e1cce20659635530aa5352f57
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-08 14:40:53 +0200

    Adding how CRUD operations should work when request is sent to the backend

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,13 @@
 -- CREATE DATABASE perntodo;
 
-CREATE TABLE todo (
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
-);
+-- CREATE TABLE todo (
+--     todo_id SERIAL PRIMARY KEY,
+--     description VARCHAR(255)
+-- );
+
+-- INSERT INTO todo (description)
+-- VALUES (
+--     'Hello world!'
+-- );
+
+SELECT * FROM todo;
